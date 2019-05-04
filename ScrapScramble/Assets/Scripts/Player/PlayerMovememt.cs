21d920d87941e955ac08abd395dbf44707d2ca74
syumai21 <bg76yf@gmail.com>
2019-05-04 19:22:56 +0900

    プレイヤーの移動、当たり判定、アクセス処理、ステータス、AIのなどのプレイヤーに関する書き換えを行う。

--- ScrapScramble/Assets/Scripts/Player/PlayerMovememt.cs
+++ ScrapScramble/Assets/Scripts/Player/PlayerMovememt.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovememt : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3f;
+    float moveX = 0f;
+    float moveZ = 0f;
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveX = Input.GetAxis("Horizontal") * speed;
+        moveZ = Input.GetAxis("Vertical") * speed;
+        Vector3 direction = new Vector3(moveX, 0, moveZ);
     }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveX, 0, moveZ);
+    }
+
 }

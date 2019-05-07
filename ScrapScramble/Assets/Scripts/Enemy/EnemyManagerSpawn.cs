6005f481fced3c2abea9c800ab2c553c8649f3e4
G-GEAR_matsuo <felid.codas@gmail.com>
2019-05-07 17:46:32 +0900

    EnemyManagerを用意。 Enemyのスポーンスクリプトを途中まで書いた。（時間計測と、スポーン回数を格納する変数の宣言・初期化）

---
+++ ScrapScramble/Assets/Scripts/Enemy/EnemyManagerSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManagerSpawn : MonoBehaviour
+{
+    //スポーンした回数
+    int spawnNum = 0;
+
+    //ゲームスタートからの時間
+    float timeFromStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNum = 0;
+
+        timeFromStart = 0.00f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ゲーム開始からの時間の計測
+        timeFromStart += Time.deltaTime;
+
+        
+    }
+}

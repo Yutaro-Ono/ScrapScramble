d52f849a21b170050cea5804f4229a14861499bf
felid <felid.codas@gmail.com>
2019-06-01 10:50:31 +0900

    Weaponブランチを作成。どう分担するかは石井君とこれから相談したい。とりあえずenumで武器を列挙。

---
+++ ScrapScramble/Assets/Scripts/Weapon/WeaponEnumDefine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Weapon
+{
+    None,           //何も持たない状態
+    Hammer,         //ハンマー
+    Gatling,        //ガトリング
+    Railgun,        //レールガン
+    Invalid         //無効値
+}
+
+/*public class WeaponEnumDefine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/

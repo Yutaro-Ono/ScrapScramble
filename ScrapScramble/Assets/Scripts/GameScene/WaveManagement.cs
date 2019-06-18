25be3f9495beb36903476d062565c4b658b16b7c
YutaroOno <ono.yutaro.tec0623@gmail.com>
2019-06-18 19:50:05 +0900

    UI上に武装のアイコンを表示するWeaponIconスクリプト、プレーヤー1,3のモデルを一新

--- ScrapScramble/Assets/Scripts/GameScene/WaveManagement.cs
+++ ScrapScramble/Assets/Scripts/GameScene/WaveManagement.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//--------------------------------------------------------//
+// GameシーンにおけるWAVEの流れを管理
+// シーンに一つだけ配置
+//-------------------------------------------------------//
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;

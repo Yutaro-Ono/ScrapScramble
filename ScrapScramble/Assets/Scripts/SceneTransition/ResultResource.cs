2e258608a1b9ef4086e91c1fa01a1cde1e50311e
takumyon <takumyon5145563@gmail.com>
2019-06-19 19:43:49 +0900

    Resourceを降らせるスクリプトを作成

---
+++ ScrapScramble/Assets/Scripts/SceneTransition/ResultResource.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultResource : MonoBehaviour
+{
+    GameObject Player1;
+    int sco = 1000;
+    PlayerStatus script; 
+    public GameObject Resource;
+    float time = 0.1f;
+   
+    void Start()
+    {
+
+    }
+    void ResourceGenerate()
+    {
+        float x = Random.Range(-70, -60);
+       
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+          
+ 
+            if (sco % 50 == 0&&sco>0)
+            {
+              
+                Vector3 CreatePoint = new Vector3(x,20,95);
+                Instantiate(Resource, CreatePoint, Quaternion.identity);
+                sco -= 50;
+            }
+           
+            time = 0.1f;
+        }
+    }
+    void Update()
+    {
+        ResourceGenerate();
+     
+    }
+}

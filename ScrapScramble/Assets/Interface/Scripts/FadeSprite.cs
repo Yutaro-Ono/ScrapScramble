2c342b338b467fcffc30832c8a02eff306aaf668
YutaroOno <ono.yutaro.tec0623@gmail.com>
2019-06-20 19:16:35 +0900

    チュートリアル用ボタンを追加、武装アイコン一新、その他interfaceを最適化

---
+++ ScrapScramble/Assets/Interface/Scripts/FadeSprite.cs
@@ -0,0 +1,75 @@
+﻿//--------------------------------------------------------//
+// Sprite画像用フェードイン・アウト演出
+//       ※フェード処理したい画像にそのままアタッチ
+//------------------------------------------------------//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeSprite : MonoBehaviour
+{
+    public float alpha = 0f;                    // アルファ値
+
+    private bool alphaSwitch;              
+
+    public Image spriteObject;        // Sprite画像格納用
+
+    // フェード処理の種類
+    public enum FadeType
+    {
+        NONE,               // 何もなし
+        FADE_IN,            // フェードイン
+        FADE_OUT,           // フェードアウト
+        FADE_LOOP           // フェードイン→アウト ループ
+    };
+
+    // 上記列挙型の取得
+    public FadeType fadeType = FadeType.NONE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeInOut();
+    }
+
+    public void FadeInOut()
+    {
+
+        if(alphaSwitch)
+        {
+            alpha += Time.deltaTime;
+        }
+
+        if(!alphaSwitch)
+        {
+            alpha -= Time.deltaTime;
+        }
+
+        SetAlpha();
+
+
+        if(alpha >= 1)
+        {
+            alphaSwitch = false;
+        }
+
+        if(alpha <= 0)
+        {
+            alphaSwitch = true;
+        }
+
+    }
+
+    void SetAlpha()
+    {
+        spriteObject.color = new Color(255, 255, 255, alpha);
+    }
+
+}

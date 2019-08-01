bfadfae88c5c11f85a9958c92197c6c9e19a2ff9
YutaroOno <ono.yutaro.tec0623@gmail.com>
2019-08-01 16:29:34 +0900

    総合的なUIの点滅処理を実装。Scene上に配置したUIManagerへBlinkUIをアタッチし、個々のUIのFadeManagerでアルファ値を参照する方式

---
+++ ScrapScramble/Assets/Interface/Scripts/BlinkUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkUI : MonoBehaviour
+{
+    private float blinkSpeed = 0.7f;        // 点滅速度
+    private float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * 3.0f * blinkSpeed;
+    }
+
+    // Alpha値を更新してColorを返す
+    public Color GetAlphaColor(Color color)
+    {
+        color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+
+        return color;
+    }
+}
---
+++ ScrapScramble/Assets/Interface/Scripts/FadeManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    private Text textObj = null;
+    private Image spriteObj = null;
+
+    GameObject UIManager;
+
+    BlinkUI blink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager = GameObject.Find("UIManager");
+        blink = UIManager.GetComponent<BlinkUI>();
+
+        textObj = this.GetComponentInParent<Text>();
+        spriteObj = this.GetComponentInParent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textObj != null)
+        {
+            textObj.color = blink.GetAlphaColor(textObj.color);
+        }
+        if (spriteObj != null)
+        {
+            spriteObj.color = blink.GetAlphaColor(spriteObj.color);
+        }
+
+    }
+}

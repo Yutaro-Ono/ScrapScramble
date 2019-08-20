f6025274c4b87fc6323d8c9e91acb77468878a34
syumai <fefa>
2019-08-20 18:48:51 +0900

    アニメーションしなくなったけどプレイヤーごとのスコア表示

--- ScrapScramble/Assets/Scripts/SceneTransition/AnimatedScore.cs
+++ ScrapScramble/Assets/Scripts/SceneTransition/AnimatedScore.cs
@@ -5,44 +5,36 @@ using UnityEngine.UI;
 public class AnimatedScore : MonoBehaviour
 {
 
-    public Text [] playerScoreText;
+    public Text playerScoreText;
 
     //プレイヤーごとのスコア
-    int []finalScore ;
+    public int []finalScore ;
+    public int PlayerNum;
 
     void Start()
     {
-        ScoreAnimation(300,2);
-    }
-
-    IEnumerator ScoreAnimation(float Score, float time)
-    {
         
-        //0fを経過時間にする
-        float elapsedTime = 0.0f;
+       
+     
+    }
+    private void Update()
+    {
+        ScoreAnimation(GameToResult.finalScore[PlayerNum], 2);
+    }
+   
+   public void ScoreAnimation( float Score,float time)
+    {
         float startTime = Time.time;
-        // 終了時間
-        float endTime = startTime + time;
-        //timeが０になるまでループさせる
-        do
+
+        float endTime = startTime +Score;
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < endTime) 
         {
             float timeRate = (Time.time - startTime) / time;
-           
-        
-            // テキストの更新
-            for (int i = 0; i < 4; i++)
-            {
-                playerScoreText[i].text = (Score  * timeRate).ToString("f0");
-            }
-
+            playerScoreText.text = (Score * timeRate).ToString();
             elapsedTime += Time.deltaTime;
-            // 0.01秒待つ
-            yield return new WaitForSeconds(0.01f);
-        } while (Time.time < endTime);
-            // 最終的な着地のスコア
-            for (int i = 0; i < 4; i++)
-        {
-            playerScoreText[i].text = Score.ToString();
-        }
+        } 
+               playerScoreText.text = Score.ToString();
     }
 }

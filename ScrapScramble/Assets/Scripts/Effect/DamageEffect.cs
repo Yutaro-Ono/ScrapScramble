ffa7e9c52c362f808728a895e3f0a33f9cd65d9c
syumai22 <bg76yf@gmail.com>
2019-08-30 21:29:16 +0900

    ダメージエフェクトの下地をつっくった

---
+++ ScrapScramble/Assets/Scripts/Effect/DamageEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEffect : MonoBehaviour
+{
+    public GameObject damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Enemy's Bullet")
+        {
+            Instantiate(damage, transform.position, transform.rotation);
+        }
+    }
+}

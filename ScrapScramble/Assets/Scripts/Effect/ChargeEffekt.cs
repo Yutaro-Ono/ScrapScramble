958f42069478c1359f738b902de0e9a1efdea2ce
syumai22 <bg76yf@gmail.com>
2019-08-30 16:19:18 +0900

    よくわからないバグが発生したけどチャージエフェクトを実装

--- ScrapScramble/Assets/Scripts/Effect/ChargeEffekt.cs
+++ ScrapScramble/Assets/Scripts/Effect/ChargeEffekt.cs
@@ -4,35 +4,64 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class ChargeEffekt : MonoBehaviour
 {
-    public EffekseerEmitter Effekseer;
     public GameObject chargeParticle;
     public Transform chagePoints;
     PlayerMovement playerMovement;
+    PlayerStatus status;
     Vector3 playePos;
+    GameObject expl;
     bool isParticlePlay;
+    float initialScale;
+    int prevScore;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
+        status = GetComponent<PlayerStatus>();
         playePos = GetComponent<Transform>().position;
+        initialScale = chargeParticle.transform.localScale.x;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Charge();
-      
-    }
-    void Charge()
-    {
-        if (playerMovement.chargeFlg == true /*&& isParticlePlay == false*/)
+        if (prevScore != status.score)
         {
-          
-                //isParticlePlay = true;
-                GameObject expl = Instantiate(chargeParticle, chagePoints) as GameObject;
-                Destroy(expl, 3.5f);
+            EffectBigger();
+        }
+        Charge();
+        prevScore = status.score;
+    }
 
+    private void Charge()
+    {
 
+        if (playerMovement.chargeFlg == true && isParticlePlay == false)
+        {
+
+            isParticlePlay = true;
+            expl = Instantiate(chargeParticle, chagePoints) as GameObject;
+
+        }
+        if (playerMovement.chargeFlg == false)
+        {
+            Destroy(expl, 0.1f);
+            isParticlePlay = false;
+        }
+
+    }
+
+    void EffectBigger()
+    {
+        // 巨大化を行う
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == status.armedStage)
+            {
+                float scale = initialScale + (4.0f * i);
+
+                chargeParticle.transform.localScale = new Vector3(scale, scale, scale);
+            }
         }
     }
 }

b5226531a48a27b3f998d99a736905c8cfdf28be
syumai22 <bg76yf@gmail.com>
2019-08-30 09:57:22 +0900

    エフェクトの実装に苦戦中

---
+++ ScrapScramble/Assets/Scripts/Effect/ChargeEffekt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class ChargeEffekt : MonoBehaviour
+{
+    public EffekseerEmitter Effekseer;
+    public GameObject chargeParticle;
+    public Transform chagePoints;
+    PlayerMovement playerMovement;
+    Vector3 playePos;
+    bool isParticlePlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        playePos = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Charge();
+      
+    }
+    void Charge()
+    {
+        if (playerMovement.chargeFlg == true /*&& isParticlePlay == false*/)
+        {
+          
+                //isParticlePlay = true;
+                GameObject expl = Instantiate(chargeParticle, chagePoints) as GameObject;
+                Destroy(expl, 3.5f);
+
+
+        }
+    }
+}

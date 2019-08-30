1cbc74d0027298fab39b8b7d2518dcc2e74819f3
syumai22 <bg76yf@gmail.com>
2019-08-30 10:40:21 +0900

    エフェクトの実装。途中まで

---
+++ ScrapScramble/Assets/Scripts/Effect/GiantEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantEffect : MonoBehaviour
+{
+    public ParticleSystem giant;
+    PlayerStatus status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        status.GetComponent<PlayerStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GiantEfe()
+    {
+       
+    }
+}

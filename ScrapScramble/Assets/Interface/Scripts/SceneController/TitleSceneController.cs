628e984e443e585ca3cf9ea379fe072e9c8f6547
G-GEAR_matsuo <felid.codas@gmail.com>
2019-09-02 12:32:12 +0900

    タイトルシーンからメニューシーンへのコントローラ遷移を実装。誰かがAボタンを押したら遷移する仕様。

--- ScrapScramble/Assets/Interface/Scripts/SceneController/TitleSceneController.cs
+++ ScrapScramble/Assets/Interface/Scripts/SceneController/TitleSceneController.cs
@@ -5,16 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class TitleSceneController : MonoBehaviour
 {
+    Rewired.Player[] players = new Rewired.Player[PlayerManagement.playerNum];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // コントローラ情報取得
+        for (int i = 0; i < PlayerManagement.playerNum; ++i)
+        {
+            players[i] = Rewired.ReInput.players.GetPlayer(i);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        // 誰か一人でもAボタンを押せばフラグを真に
+        bool anyonePressA = false;
+        for (int i = 0; i < PlayerManagement.playerNum; ++i)
+        {
+            anyonePressA = players[i].GetButton("A");
+
+            // 押されればそれ以上の走査をしない
+            if (anyonePressA)
+            {
+                break;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Space) || anyonePressA)
         {
             SceneManager.LoadScene("ChoiceMenuScene");
         }

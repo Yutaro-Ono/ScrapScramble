8c004197f8d4644ee96d751a9cc53f02b8857542
syumai22 <bg76yf@gmail.com>
2019-09-07 12:29:45 +0900

    ボタンse途中まで

---
+++ ScrapScramble/Assets/Scripts/Audio/TitleButtonAudio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleButtonAudio : MonoBehaviour
+{
+    Rewired.Player[] controllers = new Rewired.Player[PlayerManagement.playerNum];
+    public AudioClip sound;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        for (int i = 0; i < PlayerManagement.playerNum; ++i)
+        {
+            // その番号のプレイヤーがAIでなければ
+            if (ChoiceMenuSceneController.getReady[i])
+            {
+                // 操作受付
+                controllers[i] = Rewired.ReInput.players.GetPlayer(i);
+            }
+            // AIであれば
+            else
+            {
+                controllers[i] = null;
+            }
+        }
+        audioSource.clip = sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < PlayerManagement.playerNum; ++i)
+        {
+            if (controllers[i].GetButton("A"))
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
